78a1fdd8432161aff8943917ad2348e88e3ec558
cyyyy-y <327234415@qq.com>
2021-09-11 16:58:19 +0800

    Tools

--- src/views/Tools/Tools.vue
+++ src/views/Tools/Tools.vue
@@ -11,12 +11,12 @@
           <el-button type="warning" plain @click="Bytes2Str">bytes转16进制</el-button>
           <div>
             <el-input
-                style="margin-top:20px"
-                type="textarea"
-                :autosize="{ minRows: 10, maxRows: 20}"
-                resize="none"
-                placeholder=""
-                v-model="textarea1">
+                    style="margin-top:20px"
+                    type="textarea"
+                    :autosize="{ minRows: 10, maxRows: 20}"
+                    resize="none"
+                    placeholder=""
+                    v-model="textarea1">
             </el-input>
           </div>
         </el-card>
@@ -38,16 +38,14 @@
           </el-input>
           <el-button type="warning" plain @click="userDataA">userdata</el-button>
           <el-button type="warning" plain @click="userDataB">initUserData</el-button>
-<!--          <el-button type="warning" plain @click="userDataC">amountA</el-button>-->
-<!--          <el-button type="warning" plain @click="userDataD">amountB</el-button>-->
           <div>
             <el-input
-                style="margin-top:20px"
-                type="textarea"
-                :autosize="{ minRows: 10, maxRows: 20}"
-                resize="none"
-                placeholder=""
-                v-model="textarea2">
+                    style="margin-top:20px"
+                    type="textarea"
+                    :autosize="{ minRows: 10, maxRows: 20}"
+                    resize="none"
+                    placeholder=""
+                    v-model="textarea2">
             </el-input>
           </div>
         </el-card>
@@ -58,111 +56,102 @@
 </template>
 
 <script>
-import {BigNumber, ethers} from "ethers"
+  import {BigNumber, ethers} from "ethers"
+  export default {
+    name: "Tools",
+    data() {
+      return {
+        arr: '',
+        arrArray: [],
+        textarea1: '',
+        amountA: '',
+        amountB: '',
+        join: '0',
+        initialBalances: '',
+        initUserData: '',
+        textarea2: '',
+      }
+    },
+    methods: {
+      // string转bytes
+      stringToByte(str) {
+        var bytes = new Array();
+        var len, c;
+        len = str.length;
+        for (var i = 0; i < len; i++) {
+          c = str.charCodeAt(i);
+          if (c >= 0x010000 && c <= 0x10FFFF) {
+            bytes.push(((c >> 18) & 0x07) | 0xF0);
+            bytes.push(((c >> 12) & 0x3F) | 0x80);
+            bytes.push(((c >> 6) & 0x3F) | 0x80);
+            bytes.push((c & 0x3F) | 0x80);
+          } else if (c >= 0x000800 && c <= 0x00FFFF) {
+            bytes.push(((c >> 12) & 0x0F) | 0xE0);
+            bytes.push(((c >> 6) & 0x3F) | 0x80);
+            bytes.push((c & 0x3F) | 0x80);
+          } else if (c >= 0x000080 && c <= 0x0007FF) {
+            bytes.push(((c >> 6) & 0x1F) | 0xC0);
+            bytes.push((c & 0x3F) | 0x80);
+          } else {
+            bytes.push(c & 0xFF);
+          }
+        }
+        return bytes;
+      },
 
-export default {
-  name: "Tools",
-  data() {
-    return {
-      arr: '',
-      arrArray: [],
-      textarea1: '',
-      amountA: '',
-      amountB: '',
-      join: '0',
-      initialBalances: '',
-      initUserData: '',
-      textarea2: '',
+      // bytes转16进制 调用的方法
+      Bytes2Str() {
+        //去掉开头和结尾的[]
+        this.arrArray = this.arr.substring(1, this.arr.length - 1);
+        //把arr转成bytes 臭李诚误导我！！！！
+        // this.arr = this.$options.methods.stringToByte(this.arr);
+        // console.log(this.arr);
+        // console.log(this.arr.length);
+        //把arr以逗号分隔为数组
+        this.arrArray = this.arrArray.split(",");
+        //原Bytes2Str内容
+        let str = "";
+        for (let i = 0; i < this.arrArray.length; i++) {
+          let tmp = parseInt(this.arrArray[i]).toString(16);
+          if (tmp.length === 1) {
+            tmp = "0" + tmp;
+          }
+          str += tmp;
+        }
+        // 最后输出
+        this.textarea1 = "0x" + str;
+        console.log(this.textarea1);
+      },
+
+      // userDataA userdata
+      userDataA() {
+        console.log(this.join)
+        const initialBalances = [ethers.BigNumber.from("10").pow(18).mul(this.amountA), ethers.BigNumber.from("10").pow(18).mul(this.amountB)];
+        const userdata = ethers.utils.defaultAbiCoder.encode(["uint256[]"], [initialBalances]);
+        this.textarea2 = "userdata:\n\"" + userdata + "\"\n\n"
+            + "amountA:\"" + initialBalances[0].toString() + "\"\n"
+            + "amountB:\"" + initialBalances[1].toString() + "\"";
+      },
+
+      // userDataB initUserData
+      userDataB() {
+        const initialBalances = [ethers.BigNumber.from("10").pow(18).mul(this.amountA), ethers.BigNumber.from("10").pow(18).mul(this.amountB)];
+        const initUserData =
+            ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256[]'],
+                [this.join, initialBalances]);
+        this.textarea2 = "initUserData:\n\"" + initUserData + "\"\n\n"
+            + "amountA:\"" + initialBalances[0].toString() + "\"\n"
+            + "amountB:\"" + initialBalances[1].toString() + "\"";
+      },
     }
-  },
-  methods: {
-    // string转bytes
-    stringToByte(str) {
-      var bytes = new Array();
-      var len, c;
-      len = str.length;
-      for (var i = 0; i < len; i++) {
-        c = str.charCodeAt(i);
-        if (c >= 0x010000 && c <= 0x10FFFF) {
-          bytes.push(((c >> 18) & 0x07) | 0xF0);
-          bytes.push(((c >> 12) & 0x3F) | 0x80);
-          bytes.push(((c >> 6) & 0x3F) | 0x80);
-          bytes.push((c & 0x3F) | 0x80);
-        } else if (c >= 0x000800 && c <= 0x00FFFF) {
-          bytes.push(((c >> 12) & 0x0F) | 0xE0);
-          bytes.push(((c >> 6) & 0x3F) | 0x80);
-          bytes.push((c & 0x3F) | 0x80);
-        } else if (c >= 0x000080 && c <= 0x0007FF) {
-          bytes.push(((c >> 6) & 0x1F) | 0xC0);
-          bytes.push((c & 0x3F) | 0x80);
-        } else {
-          bytes.push(c & 0xFF);
-        }
-      }
-      return bytes;
-    },
-    // bytes转16进制 调用的方法
-    Bytes2Str() {
-      //去掉开头和结尾的[]
-      this.arrArray = this.arr.substring(1, this.arr.length - 1);
-      //把arr转成bytes 臭李诚误导我！！！！
-      // this.arr = this.$options.methods.stringToByte(this.arr);
-      // console.log(this.arr);
-      // console.log(this.arr.length);
-      //把arr以逗号分隔为数组
-      this.arrArray = this.arrArray.split(",");
-      //原Bytes2Str内容
-      let str = "";
-      for (let i = 0; i < this.arrArray.length; i++) {
-        let tmp = parseInt(this.arrArray[i]).toString(16);
-        if (tmp.length === 1) {
-          tmp = "0" + tmp;
-        }
-        str += tmp;
-      }
-      // 最后输出
-      this.textarea1 = "0x" + str;
-      console.log(this.textarea1);
-    },
-    // userDataA userdata
-    userDataA() {
-      console.log(this.join)
-      const initialBalances = [ethers.BigNumber.from("10").pow(18).mul(this.amountA), ethers.BigNumber.from("10").pow(18).mul(this.amountB)];
-      const userdata = ethers.utils.defaultAbiCoder.encode(["uint256[]"], [initialBalances]);
-      this.textarea2 = userdata
-      // this.textarea2 = "userdata:" + userdata
-    },
-    // userDataB initUserData
-    userDataB() {
-      const initialBalances = [ethers.BigNumber.from("10").pow(18).mul(this.amountA), ethers.BigNumber.from("10").pow(18).mul(this.amountB)];
-      const initUserData =
-          ethers.utils.defaultAbiCoder.encode(['uint256', 'uint256[]'],
-              [this.join, initialBalances]);
-
-      this.textarea2 = "initUserData:\n\"" + initUserData + "\"\n\n"
-      + "amountA:\"" + initialBalances[0].toString() + "\"\n"
-      + "amountB:\"" + initialBalances[1].toString() + "\"";
-    },
-    // userDataC amountA
-    userDataC() {
-      const initialBalances = [ethers.BigNumber.from("10").pow(18).mul(this.amountA), ethers.BigNumber.from("10").pow(18).mul(this.amountB)];
-      this.textarea2 = "amountA:" + initialBalances[0].toString()
-    },
-    // userDataD amountB
-    userDataD() {
-      const initialBalances = [ethers.BigNumber.from("10").pow(18).mul(this.amountA), ethers.BigNumber.from("10").pow(18).mul(this.amountB)];
-      this.textarea2 = "amountB:" + initialBalances[1].toString()
-    },
   }
-}
 </script>
 
 <style scoped>
-.el-row {
-  margin-top: 1rem;
-}
-
-.el-input, .el-button {
-  margin-top: 1rem;
-}
+  .el-row {
+    margin-top: 1rem;
+  }
+  .el-input, .el-button {
+    margin-top: 1rem;
+  }
 </style>
